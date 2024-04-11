ba5930ad7c39b60059b3cc6896316a5eb9b6bd5c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-04-11 14:11:06 -0400

    chore: Add the lcsfinder/lcsfinder/main.py file.

---
+++ lcsfinder/lcsfinder/main.py
@@ -0,0 +1,6 @@
+"""Conduct experiments to evaluate performance of LCS computation."""
+
+# TODO: Add all of the required source code to implement a benchmark
+# that runs a doubling experiment to evaluate the performance of the
+# three algorithms that compute the longest common subsequence (LCS)
+# of two strings that contain either characters or numbers.
