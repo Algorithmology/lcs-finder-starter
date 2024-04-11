592019fc73baf44f4489c4d6e17b6d34cb9c555a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-04-11 14:21:29 -0400

    test: Add the test_constants.py with TODO markers for all tests.

---
+++ lcsfinder/tests/test_constants.py
@@ -0,0 +1,14 @@
+"""Test suite for the constants module."""
+
+# ruff: noqa: PLR2004
+
+# TODO: implement test cases for any of the constants.
+#
+# TODO: You also need to implement test cases for:
+# - The constant enumeration(s) in the approach module.
+# - All of the methods associated data generation
+# - All of the methods associated running a benchmark
+# - All of the methods associated the LCS algorithms
+#
+# Ultimately, you should have a test suite for each of
+# the individual files inside of the lcsfinder directory.
